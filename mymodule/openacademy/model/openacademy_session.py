33f58a8646cfedf560e89b8437881edde1d85e7f
root <igr@soluciones4g.com>
2017-05-18 23:18:19 +0000

    [REF] openacademy: Adding m2m to view and model

--- mymodule/openacademy/model/openacademy_session.py
+++ mymodule/openacademy/model/openacademy_session.py
@@ -10,3 +10,4 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one("res.partner", string="Instructor")
     course_id = fields.Many2one("openacademy.course", ondelete="cascade", string="Course", required=True)
+    attendees_ids = fields.Many2many("res.partner", string="Attendees")
