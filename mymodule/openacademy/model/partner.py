2072a96de77127944240695f831dae71742d2d8c
root <igr@soluciones4g.com>
2017-05-19 02:01:31 +0000

    [REF] openacademy: Adding Partner at models

---
+++ mymodule/openacademy/model/partner.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    instructor = fields.Boolean(default=False)
+    session_ids = fields.Many2many("openacademy.session", string="Session as instructor", readonly=True)
