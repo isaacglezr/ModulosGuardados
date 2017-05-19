ad5e44c9a2b76c7a325f0864250fd9f2d2c5756b
root <igr@soluciones4g.com>
2017-05-19 17:19:28 +0000

    [REF] openacademy: Adding Customers menu and kanban

--- mymodule/openacademy/model/partner.py
+++ mymodule/openacademy/model/partner.py
@@ -4,4 +4,4 @@ class Partner(models.Model):
     _inherit = "res.partner"
 
     instructor = fields.Boolean(default=False)
-    session_ids = fields.Many2many("openacademy.session", string="Session as instructor", readonly=True)
+    session_ids = fields.Many2many("openacademy.session", string="Session as attendee", readonly=True)
