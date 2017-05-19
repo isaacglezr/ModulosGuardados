80634aadfaad98b73751a2c0205e69cf56fc280e
root <igr@soluciones4g.com>
2017-05-19 23:25:57 +0000

    [REF] openacademy: Adding Computer attribute

--- mymodule/openacademy/model/openacademy_session.py
+++ mymodule/openacademy/model/openacademy_session.py
@@ -11,3 +11,13 @@ class Session(models.Model):
     instructor_id = fields.Many2one("res.partner", string="Instructor", domain=['|',("instructor","=",True),('category_id.name', "ilike", "Teacher")])
     course_id = fields.Many2one("openacademy.course", ondelete="cascade", string="Course", required=True)
     attendees_ids = fields.Many2many("res.partner", string="Attendees")
+
+    taken_seats = fields.Float(string="Taken seats", compute="_taken_seats")
+
+    @api.one
+    @api.depends('seats', 'attendee_id')
+    def _taken_seats(self):
+        if not self.seats:
+            self.taken_seats = 0
+        else:
+            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
